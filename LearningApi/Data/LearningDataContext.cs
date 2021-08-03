a754d1be3285c95a4f9e92ee752e1ff7e5065794
Ikromer-SE <ikromermobile@gmail.com>
2021-08-03 16:31:44 -0400

    Add project files.

---
+++ LearningApi/Data/LearningDataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearningApi.Data
+{
+    public class LearningDataContext: DbContext
+    {
+        public LearningDataContext(DbContextOptions<LearningDataContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
